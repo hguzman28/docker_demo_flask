979ba2e1aae134bf37adcbd720a478dd4eef16d0
Henner Guzman <hguzman0028@gmail.com>
2022-02-24 08:55:59 -0500

    limpieza del repo

--- clientes/models.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify, request
-
-class Mclientes:
-  
-    def return_clientes(self):
-        clientes = {'nombre':'Henner', 'apellido':'guzman'}
-        return clientes
